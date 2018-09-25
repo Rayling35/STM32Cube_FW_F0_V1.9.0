8b6dd1a998f6e2ffba1cb0259b9dae56de096387
Rayling <honwe.lin@gmail.com>
2018-09-25 23:45:50 +0800

    [Update] Project of arch_driver_pz_lcd some define modify

--- Projects/STM32F030R8-Nucleo/Examples/MY_LIB/arch_driver/hal/gpio_driver.c
+++ Projects/STM32F030R8-Nucleo/Examples/MY_LIB/arch_driver/hal/gpio_driver.c
@@ -2,62 +2,65 @@
  * SPDX-License-Identifier: MIT
  */
 #include "stm32f0xx_hal.h"
+#ifdef RTOS_ON
 #include "cmsis_os.h"
+#endif
 #include "device.h"
 #include "api_define.h"
 #include "gpio_driver.h"
 #include "api_gpio_common.h"
 
 
-static int read_data(struct device *dev)
+static int read_data(struct device *Dev)
 {
-	struct gpio_data *d_data  = dev->data;
-	struct gpio_api *gpio_hal = d_data->gpio_hal;
+	struct gpio_data *D_data  = Dev->data;
+	struct gpio_api *Gpio_hal = D_data->Gpio_hal;
 	
-	return gpio_hal->read();
+	return Gpio_hal->read();
 }
 
-static void write_data(struct device *dev, uint16_t state)
+static void write_data(struct device *Dev, uint16_t state)
 {
-	struct gpio_data *d_data  = dev->data;
-	struct gpio_api *gpio_hal = d_data->gpio_hal;
+	struct gpio_data *D_data  = Dev->data;
+	struct gpio_api *Gpio_hal = D_data->Gpio_hal;
 	
-	gpio_hal->write(state);
+	Gpio_hal->write(state);
 }
 
-static void toggle_write_data(struct device *dev)
+static void toggle_write_data(struct device *Dev)
 {
-	struct gpio_data *d_data  = dev->data;
-	struct gpio_api *gpio_hal = d_data->gpio_hal;
+	struct gpio_data *D_data  = Dev->data;
+	struct gpio_api *Gpio_hal = D_data->Gpio_hal;
 	
-	gpio_hal->toggle_write();
+	Gpio_hal->toggle_write();
 }
 
-static uint32_t press_time_data(struct device *dev, enum count_unit e_unit)
+#ifdef RTOS_ON
+static uint32_t press_time_data(struct device *Dev, enum count_unit e_unit)
 {
-	struct gpio_data *d_data  = dev->data;
-	struct gpio_api *gpio_hal = d_data->gpio_hal;
+	struct gpio_data *D_data  = Dev->data;
+	struct gpio_api *Gpio_hal = D_data->Gpio_hal;
 	int no_press_status;
 	uint32_t count;
 	
-	no_press_status = gpio_hal->read();
+	no_press_status = Gpio_hal->read();
 	
 	switch (e_unit) {
 		case MILLISECOND:
 			while(1) {
-				if(gpio_hal->read() != no_press_status) {
+				if(Gpio_hal->read() != no_press_status) {
 					count = osKernelSysTick();
 					do {
-					}while(gpio_hal->read() != no_press_status);
+					}while(Gpio_hal->read() != no_press_status);
 					return osKernelSysTick() - count;
 				}
 			}
 		case SECOND:
 			while(1) {
-				if(gpio_hal->read() != no_press_status) {
+				if(Gpio_hal->read() != no_press_status) {
 					count = osKernelSysTick();
 					do {
-					}while(gpio_hal->read() != no_press_status);
+					}while(Gpio_hal->read() != no_press_status);
 					return (osKernelSysTick() - count) / 1000;
 				}
 			}
@@ -65,8 +68,14 @@ static uint32_t press_time_data(struct device *dev, enum count_unit e_unit)
 			return 0;
 	}
 }
+#else
+static uint32_t press_time_data(struct device *Dev, enum count_unit e_unit)
+{
+	return 0;
+}
+#endif
 
-static const struct gpio_common_api gpio_common_api = {
+static const struct gpio_common_api Gpio_common_api = {
 	.read         = read_data,
 	.write        = write_data,
 	.toggle_write = toggle_write_data,
@@ -76,28 +85,28 @@ static const struct gpio_common_api gpio_common_api = {
 
 #ifdef GPIO_B2_DEV
 #include "gpio_b2.h"
-static struct gpio_data gpio_b2_data;
+static struct gpio_data Gpio_b2_data;
 
-static int gpio_b2_dev_init(struct device *dev)
+static int gpio_b2_device_init(struct device *Dev)
 {
-	struct gpio_data *d_data = dev->data;
+	struct gpio_data *D_data = Dev->data;
 	
-	d_data->gpio_hal = gpio_b2_binding();
-	d_data->gpio_hal->init();
+	D_data->Gpio_hal = gpio_b2_binding();
+	D_data->Gpio_hal->init();
 	printf("GPIO_B2 device init\r\n");
 
 	return 0;
 }
 
-struct device gpio_b2 = {
-	.api    = &gpio_common_api,
-	.data   = &gpio_b2_data,
-	.init   = gpio_b2_dev_init,
+struct device Gpio_b2 = {
+	.api  = &Gpio_common_api,
+	.data = &Gpio_b2_data,
+	.init = gpio_b2_device_init,
 };
 
 struct device* gpio_b2_device_binding(void)
 {
-	return &gpio_b2;
+	return &Gpio_b2;
 }
 
 #ifdef B2_EXTI
@@ -110,28 +119,28 @@ __weak void b2_exti_handel(void)
 
 #ifdef GPIO_C5_DEV
 #include "gpio_c5.h"
-static struct gpio_data gpio_c5_data;
+static struct gpio_data Gpio_c5_data;
 
-static int gpio_c5_dev_init(struct device *dev)
+static int gpio_c5_device_init(struct device *Dev)
 {
-	struct gpio_data *d_data = dev->data;
+	struct gpio_data *D_data = Dev->data;
 	
-	d_data->gpio_hal = gpio_c5_binding();
-	d_data->gpio_hal->init();
+	D_data->Gpio_hal = gpio_c5_binding();
+	D_data->Gpio_hal->init();
 	printf("GPIO_C5 device init\r\n");
 
 	return 0;
 }
 
-struct device gpio_c5 = {
-	.api    = &gpio_common_api,
-	.data   = &gpio_c5_data,
-	.init   = gpio_c5_dev_init,
+struct device Gpio_c5 = {
+	.api  = &Gpio_common_api,
+	.data = &Gpio_c5_data,
+	.init = gpio_c5_device_init,
 };
 
 struct device* gpio_c5_device_binding(void)
 {
-	return &gpio_c5;
+	return &Gpio_c5;
 }
 
 #ifdef C5_EXTI
@@ -144,28 +153,28 @@ __weak void c5_exti_handel(void)
 
 #ifdef GPIO_C6_DEV
 #include "gpio_c6.h"
-static struct gpio_data gpio_c6_data;
+static struct gpio_data Gpio_c6_data;
 
-static int gpio_c6_dev_init(struct device *dev)
+static int gpio_c6_device_init(struct device *Dev)
 {
-	struct gpio_data *d_data = dev->data;
+	struct gpio_data *D_data = Dev->data;
 	
-	d_data->gpio_hal = gpio_c6_binding();
-	d_data->gpio_hal->init();
+	D_data->Gpio_hal = gpio_c6_binding();
+	D_data->Gpio_hal->init();
 	printf("GPIO_C6 device init\r\n");
 
 	return 0;
 }
 
-struct device gpio_c6 = {
-	.api    = &gpio_common_api,
-	.data   = &gpio_c6_data,
-	.init   = gpio_c6_dev_init,
+struct device Gpio_c6 = {
+	.api  = &Gpio_common_api,
+	.data = &Gpio_c6_data,
+	.init = gpio_c6_device_init,
 };
 
 struct device* gpio_c6_device_binding(void)
 {
-	return &gpio_c6;
+	return &Gpio_c6;
 }
 
 #ifdef C6_EXTI
@@ -178,28 +187,28 @@ __weak void c6_exti_handel(void)
 
 #ifdef GPIO_C8_DEV
 #include "gpio_c8.h"
-static struct gpio_data gpio_c8_data;
+static struct gpio_data Gpio_c8_data;
 
-static int gpio_c8_dev_init(struct device *dev)
+static int gpio_c8_device_init(struct device *Dev)
 {
-	struct gpio_data *d_data = dev->data;
+	struct gpio_data *D_data = Dev->data;
 	
-	d_data->gpio_hal = gpio_c8_binding();
-	d_data->gpio_hal->init();
+	D_data->Gpio_hal = gpio_c8_binding();
+	D_data->Gpio_hal->init();
 	printf("GPIO_C8 device init\r\n");
 
 	return 0;
 }
 
-struct device gpio_c8 = {
-	.api    = &gpio_common_api,
-	.data   = &gpio_c8_data,
-	.init   = gpio_c8_dev_init,
+struct device Gpio_c8 = {
+	.api  = &Gpio_common_api,
+	.data = &Gpio_c8_data,
+	.init = gpio_c8_device_init,
 };
 
 struct device* gpio_c8_device_binding(void)
 {
-	return &gpio_c8;
+	return &Gpio_c8;
 }
 
 #ifdef C8_EXTI
@@ -212,28 +221,28 @@ __weak void c8_exti_handel(void)
 
 #ifdef GPIO_C13_DEV
 #include "gpio_c13.h"
-static struct gpio_data gpio_c13_data;
+static struct gpio_data Gpio_c13_data;
 
-static int gpio_c13_dev_init(struct device *dev)
+static int gpio_c13_device_init(struct device *Dev)
 {
-	struct gpio_data *d_data = dev->data;
+	struct gpio_data *D_data = Dev->data;
 	
-	d_data->gpio_hal = gpio_c13_binding();
-	d_data->gpio_hal->init();
+	D_data->Gpio_hal = gpio_c13_binding();
+	D_data->Gpio_hal->init();
 	printf("GPIO_C13 device init\r\n");
 
 	return 0;
 }
 
-struct device gpio_c13 = {
-	.api    = &gpio_common_api,
-	.data   = &gpio_c13_data,
-	.init   = gpio_c13_dev_init,
+struct device Gpio_c13 = {
+	.api  = &Gpio_common_api,
+	.data = &Gpio_c13_data,
+	.init = gpio_c13_device_init,
 };
 
 struct device* gpio_c13_device_binding(void)
 {
-	return &gpio_c13;
+	return &Gpio_c13;
 }
 
 #ifdef C13_EXTI
